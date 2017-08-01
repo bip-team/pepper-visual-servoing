3c8138001ebe19a9eac1e4d31f98eece0bbcdc59
jmichalczyk <jasiekmi@wp.pl>
2017-08-01 10:46:45 +0200

    add ModelBasedTracker class

---
+++ pepper-visp/model_based_tracker.h
@@ -0,0 +1,99 @@
+/**
+    @file
+    @author Jan Michalczyk 
+
+    @brief
+*/
+
+#pragma once
+
+#include <visp3/gui/vpDisplayGDI.h>
+#include <visp3/gui/vpDisplayOpenCV.h>
+#include <visp3/gui/vpDisplayX.h>
+#include <visp3/io/vpImageIo.h>
+#include <visp3/core/vpIoTools.h>
+#include <visp3/mbt/vpMbEdgeTracker.h>
+#include <visp3/mbt/vpMbEdgeKltTracker.h>
+#include <visp3/io/vpVideoReader.h>
+
+#include <boost/pointer_cast.hpp>
+
+namespace pepper_visp
+{
+    class ModelBasedTracker 
+    {
+        public:
+            ModelBasedTracker(const std::string&        data_path, 
+                              const vpCameraParameters& camera_parameters,
+                              const std::string&        model_filename) : data_path_(data_path),
+                                                                          camera_parameters_(camera_parameters)
+            {
+                tracker_.reset(new vpMbEdgeKltTracker);
+                initializeTrackerSettings();
+                camera_parameters_.initPersProjWithoutDistortion(839, 839, 325, 243);
+                tracker_->setCameraParameters(camera_parameters_);
+                tracker_->loadModel(data_path_ + model_filename);
+                tracker_->setDisplayFeatures(true);
+            }
+
+
+            void initializeByClick(const vpImage<unsigned char>& image, 
+                                   const std::string& init_filename)
+            {
+                tracker_->initClick(image, data_path_ + init_filename, true);
+            }
+
+
+            void track(const vpImage<unsigned char>& image)
+            {
+                tracker_->track(image);
+            }
+
+            
+            void getPose(vpHomogeneousMatrix& cMo)
+            {
+                tracker_->getPose(cMo);
+            }
+            
+
+            void display(const vpImage<unsigned char>& image, 
+                         const vpHomogeneousMatrix&    cMo)
+            {
+                tracker_->display(image, cMo, camera_parameters_, vpColor::red, 2, true);
+            }
+
+
+        private:  
+            void initializeTrackerSettings()
+            {
+                vpMe moving_edge;
+                moving_edge.setMaskSize(5);
+                moving_edge.setMaskNumber(180);
+                moving_edge.setRange(8);
+                moving_edge.setThreshold(10000);
+                moving_edge.setMu1(0.5);
+                moving_edge.setMu2(0.5);
+                moving_edge.setSampleStep(4);
+                
+                boost::dynamic_pointer_cast<vpMbEdgeTracker>(tracker_)->setMovingEdge(moving_edge);        
+
+                vpKltOpencv klt_settings;
+                klt_settings.setMaxFeatures(300);
+                klt_settings.setWindowSize(5);
+                klt_settings.setQuality(0.015);
+                klt_settings.setMinDistance(8);
+                klt_settings.setHarrisFreeParameter(0.01);
+                klt_settings.setBlockSize(3);
+                klt_settings.setPyramidLevels(3);
+                
+                boost::dynamic_pointer_cast<vpMbKltTracker>(tracker_)->setKltOpencv(klt_settings);
+                boost::dynamic_pointer_cast<vpMbKltTracker>(tracker_)->setKltMaskBorder(5);
+            }
+
+
+        private:
+            std::string                           data_path_;
+            vpCameraParameters                    camera_parameters_;
+            boost::shared_ptr<vpMbEdgeKltTracker> tracker_;
+    };
+}//pepper_visp
--- pepper-visp/model_based_tracker_servo.cpp
+++ pepper-visp/model_based_tracker_servo.cpp
@@ -6,17 +6,7 @@
 */
 
 #include "pepper_visp.h"
-
-#include <visp3/gui/vpDisplayGDI.h>
-#include <visp3/gui/vpDisplayOpenCV.h>
-#include <visp3/gui/vpDisplayX.h>
-#include <visp3/io/vpImageIo.h>
-#include <visp3/core/vpIoTools.h>
-#include <visp3/mbt/vpMbEdgeTracker.h>
-#include <visp3/mbt/vpMbEdgeKltTracker.h>
-#include <visp3/io/vpVideoReader.h>
-
-#include <boost/pointer_cast.hpp>
+#include "model_based_tracker.h"
 
 int main(int argc, char** argv)
 {
@@ -25,60 +15,33 @@ int main(int argc, char** argv)
         // connect to the robot
         pepper_visp::PepperVS pepper_vs(PEPPER_VISP_FOREHEAD_CONFIG_FILE);
 
-        // get intrinsic parameters
-        vpCameraParameters camera_parameters = pepper_vs.getIntrinsicCameraParameters();
-
         // object pose in camera frame
         vpHomogeneousMatrix cMo;
+        vpCameraParameters  camera_parameters =
+                                pepper_vs.getIntrinsicCameraParameters();
 
         // get image display
         vpImage<unsigned char> image(pepper_vs.getImageHeight(), pepper_vs.getImageWidth());
         vpDisplayX d(image);
         vpDisplay::setTitle(image, "ViSP viewer");
         
+        pepper_vs.getImage(image);
+        vpDisplay::display(image);
+
         // get tracker
-        boost::shared_ptr<vpMbEdgeKltTracker> tracker(new vpMbEdgeKltTracker);
-
-            pepper_vs.getImage(image);
-            vpDisplay::display(image);
+        pepper_visp::ModelBasedTracker tracker(PEPPER_VISP_DATA_PATH,
+                                               camera_parameters,
+                                               "greenbox.cao");
         
-        // tracker settings
-        vpMe moving_edge;
-        moving_edge.setMaskSize(5);
-        moving_edge.setMaskNumber(180);
-        moving_edge.setRange(8);
-        moving_edge.setThreshold(10000);
-        moving_edge.setMu1(0.5);
-        moving_edge.setMu2(0.5);
-        moving_edge.setSampleStep(4);
-        boost::dynamic_pointer_cast<vpMbEdgeTracker>(tracker)->setMovingEdge(moving_edge);        
-
-        vpKltOpencv klt_settings;
-        klt_settings.setMaxFeatures(300);
-        klt_settings.setWindowSize(5);
-        klt_settings.setQuality(0.015);
-        klt_settings.setMinDistance(8);
-        klt_settings.setHarrisFreeParameter(0.01);
-        klt_settings.setBlockSize(3);
-        klt_settings.setPyramidLevels(3);
-        boost::dynamic_pointer_cast<vpMbKltTracker>(tracker)->setKltOpencv(klt_settings);
-        boost::dynamic_pointer_cast<vpMbKltTracker>(tracker)->setKltMaskBorder(5);
-      
-        camera_parameters.initPersProjWithoutDistortion(839, 839, 325, 243);
-        tracker->setCameraParameters(camera_parameters);
-        
-        tracker->loadModel(std::string(PEPPER_VISP_DATA_PATH) + "greenbox.cao");
-        tracker->setDisplayFeatures(true);
-        tracker->initClick(image, std::string(PEPPER_VISP_DATA_PATH) + "greenbox.init", true);
+        tracker.initializeByClick(image, "greenbox.init");
 
         while(true)
         {
             pepper_vs.getImage(image);
             vpDisplay::display(image);
-            tracker->track(image);
-            tracker->getPose(cMo);
-            tracker->getCameraParameters(camera_parameters);
-            tracker->display(image, cMo, camera_parameters, vpColor::red, 2, true);
+            tracker.track(image);
+            tracker.getPose(cMo);
+            tracker.display(image, cMo);
             vpDisplay::displayFrame(image, cMo, camera_parameters, 0.025, vpColor::none, 3);
             vpDisplay::displayText(image, 10, 10, "A click to exit...", vpColor::red);
             vpDisplay::flush(image);
