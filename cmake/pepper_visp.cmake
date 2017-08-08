68188f597d69719719907b50dbcdfa5c81c66123
jmichalczyk <jasiekmi@wp.pl>
2017-08-08 09:59:56 +0200

    add config-related variable in cmake

--- cmake/pepper_visp.cmake
+++ cmake/pepper_visp.cmake
@@ -1,5 +1,6 @@
 #set(PEPPER_VISP_INTRINSIC_CAMERA_FILE "/home/jasiek/pepper-workspace/pepper-visual-servoing/config/pepper_cameras.xml"             CACHE STRING "")
 #set(PEPPER_VISP_FOREHEAD_CONFIG_FILE  "/home/jasiek/pepper-workspace/pepper-visual-servoing/config/pepper_vs_forehead_config.yaml" CACHE STRING "")
+#set(PEPPER_VISP_CHIN_CONFIG_FILE      "/home/jasiek/pepper-workspace/pepper-visual-servoing/config/pepper_vs_chin_config.yaml"     CACHE STRING "")
 #set(PEPPER_VISP_DATA_PATH             "/home/jasiek/pepper-workspace/pepper-visual-servoing/data/"                                 CACHE STRING "")
 #set(PEPPER_VISP_LOG_VELOCITY          OFF                                                                                          CACHE BOOL   "")
 #set(PEPPER_VISP_USE_PEPPER_CONTROLLER ON                                                                                           CACHE BOOL   "")
