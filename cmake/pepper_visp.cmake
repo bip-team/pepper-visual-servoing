9beb4c2e7bcaec465e5fcafba94319b07df41048
jmichalczyk <jasiekmi@wp.pl>
2017-08-22 14:34:24 +0200

    up pepper_visp.cmake

--- cmake/pepper_visp.cmake
+++ cmake/pepper_visp.cmake
@@ -1,3 +1,4 @@
+#set(PEPPER_VISP_CONFIG_PATH           "/home/jasiek/pepper-workspace/pepper-visual-servoing/config/"                               CACHE STRING "")
 #set(PEPPER_VISP_INTRINSIC_CAMERA_FILE "/home/jasiek/pepper-workspace/pepper-visual-servoing/config/pepper_cameras.xml"             CACHE STRING "")
 #set(PEPPER_VISP_FOREHEAD_CONFIG_FILE  "/home/jasiek/pepper-workspace/pepper-visual-servoing/config/pepper_vs_forehead_config.yaml" CACHE STRING "")
 #set(PEPPER_VISP_CHIN_CONFIG_FILE      "/home/jasiek/pepper-workspace/pepper-visual-servoing/config/pepper_vs_chin_config.yaml"     CACHE STRING "")
